b9a6dbfaed3ed41830d6c2bbc0425c3199b106a4
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2022-11-27 21:54:08 +0530

    Create Iterative Inorder Traversal.cpp

---
+++ Iterative Inorder Traversal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        if(root == NULL) return inorder;
+        stack<TreeNode *> st;
+        TreeNode* node = root;
+        while(true){
+            if(node != NULL)
+            {
+                st.push(node);
+                node = node -> left;
+            }
+            else
+            {
+                if(st.empty() == true) break;
+                node = st.top();
+                st.pop();
+                inorder.push_back(node -> val);
+                node = node -> right;
+            }
+        }
+        return inorder;
+    }
+};
