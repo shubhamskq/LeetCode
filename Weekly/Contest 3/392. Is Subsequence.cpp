7edbe89349ec39460ad12888c9ff5a9e194a900b
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2023-02-18 14:51:54 +0530

    Update and rename New Text Document.txt to 392. Is Subsequence.cpp

--- Weekly/Contest 3/392. Is Subsequence.cpp
+++ Weekly/Contest 3/392. Is Subsequence.cpp
@@ -16,3 +16,20 @@ public:
         return isSubSeq(s,t,m,n);
     }
 };
+
+class Solution {
+public:
+    //Using Two Pointers:
+    bool isSubsequence(string s, string t) {
+      int m = s.size();
+      int n = t.size();
+        int i = 0 , j = 0;
+        while(i < m && j < n){
+            if(s[i] == t[j]){
+                i++;
+            }
+            j++;
+        }
+    return i == m ? 1 : 0;
+    }
+};
