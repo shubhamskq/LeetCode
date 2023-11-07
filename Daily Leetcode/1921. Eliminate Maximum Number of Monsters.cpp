a2a3c1ea1b85c1c188497851558633a7034af845
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2023-11-07 14:54:18 +0530

    Create 1921. Eliminate Maximum Number of Monsters.cpp

---
+++ Daily Leetcode/1921. Eliminate Maximum Number of Monsters.cpp
@@ -0,0 +1,24 @@
+// Greedy Approach
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int n = dist.size();
+        vector<double> time(n);
+        for(int i = 0 ; i < n ; i++)
+        {
+            time[i] = static_cast<double>(dist[i])/speed[i];
+        }
+
+        sort(time.begin(), time.end());
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(time[i] <= i)
+            {
+                return i;
+            }
+        }
+      return n;
+    }
+};
+
+// Best time complexity with counting sort
